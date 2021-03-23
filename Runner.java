566adbffd33cb64d40e1f247af2f903d8d46b461
mahadsarfraz <63406407+mahadsarfraz@users.noreply.github.com>
2021-03-23 22:40:48 +0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,39 +1,9 @@
-import java.util.Scanner;
-
 
 public class Runner {
 	public static void main(String[] args) {
+		Fraction fraction = new Fraction(2,4);
+		fraction.ratio();
+		fraction.equals(2,4);
 		
-		int Id = -1;
-		Scanner myVar = new Scanner(System.in);
-		System.out.print("first stand: ");
-		HotDogStand H1 = new HotDogStand();
-		System.out.print("second stand: ");
-		HotDogStand H2 = new HotDogStand();
-		System.out.print("third stand: ");
-		HotDogStand H3 = new HotDogStand();
-		
-		
-		while(Id != 0 ) {
-			System.out.print("Enter Id of HotDogStand : ");
-		Id = myVar.nextInt();
-			
-			if (Id == H1.Id) {
-				H1.JustSold();
-			}
-			else if(Id == H2.Id) {
-				H2.JustSold();
-			}
-			else if(Id == H3.Id) {
-				H3.JustSold();
-			}
-			else if(Id == 0) {
-				System.out.print("Your program has been terminated");
-			}
-			else {
-				System.out.println("Enter correct Id or 0 to terminate");
-			}
-			
-		}
 	}
 }
